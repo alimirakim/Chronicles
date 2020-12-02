5455ec7b1365e595bcdf70ee4f9ae5f355afd1e5
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-12-02 10:19:47 -0500

    Initial commit, using Bart's flask-react starter.

---
+++ frontend/src/components/auth/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = props => {
+
+  if (!props.authenticated) return <Redirect to="/login"/>
+
+  return <Route {...props}/>
+};
+
+export default ProtectedRoute;
